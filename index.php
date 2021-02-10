5c8ba8dc8f665e40d4221f3948148df7b24f0be0
Emilien <emilien.fuchs@gmail.com>
2021-02-10 17:20:43 +0100

    Ajout tpl + modification header
    Rendu dynamique du header sur chaque pages
    et ajout contact.tpl

--- index.php
+++ index.php
@@ -11,6 +11,14 @@ else
     require(_CTRL_.'index.php');
 
 
+//Modification du header en fonction du nom/type de la page
+if (isset($_GET['page']) && file_exists(_TPL_.$_GET['page'].'.tpl')){
+    $smarty->assign('pagename',$_GET['page'].' small');
+} else {
+    $smarty->assign('pagename','');
+}
+
+
 // Affichage des templates
 $smarty->display('template/header.tpl');
 
