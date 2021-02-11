fc308ce3a9ba63d4440bef0bbb35dc86a51dc489
Emilien <emilien.fuchs@gmail.com>
2021-02-11 11:42:29 +0100

    Bug mineur

--- config/config_init.php
+++ config/config_init.php
@@ -7,5 +7,14 @@ require('defines.inc.php');
 require('libs/Smarty.class.php');
 require ('config_bdd.php');
 
+//connexion à la bdd
+try {
+    $bdd = [
+        'bdd' => new PDO('mysql:dbname='.$bdd.';host='.$server.'', $user, $password)
+    ];
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+
 // Initialisation Smarty
 $smarty = new Smarty();
