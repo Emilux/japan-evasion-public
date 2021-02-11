08dd8c664d9a67f8aad2cb5892b4c0e57d01f9a4
J-HERTH <73234582+J-HERTH@users.noreply.github.com>
2021-02-11 16:48:28 +0100

    Création des models

---
+++ models/Administrateur.php
@@ -0,0 +1,18 @@
+<?php
+
+class Administrateur extends Moderateur {
+
+    public function bannirModerateur(){
+
+    }
+
+    public function retrograderModerateur(){
+
+    }
+
+    public function promModerateur(){
+
+    }
+
+
+}
