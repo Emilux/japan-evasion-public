650f5232e97aa608020a0f7a19c64752238e9c13
Emilien <emilien.fuchs@gmail.com>
2021-02-15 15:31:30 +0100

    Modification header

--- index.php
+++ index.php
@@ -10,14 +10,6 @@ if (isset($_GET['page']) && file_exists(_CTRL_.str_replace('.', '', ucfirst($_GE
 else
     require(_CTRL_.'IndexController.php');
 
-
-//Modification du header en fonction du nom/type de la page
-if (isset($_GET['page']) && file_exists(_TPL_.$_GET['page'].'.tpl')){
-    $smarty->assign('pagename',$_GET['page'].' small');
-} else {
-    $smarty->assign('pagename','');
-}
-
 // Charge Controller de la création ou connexion à son compte sur
 // Toutes les pages du site
 require (_CTRL_.'CompteController.php');
