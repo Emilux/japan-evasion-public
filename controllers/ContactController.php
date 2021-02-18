9213d828fddb8f652dd03abc66e4175fa0069953
Sadaf <sadafmirzad0@gmail.com>
2021-02-18 15:58:26 +0100

    Ajout contact

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -1,3 +1,18 @@
 <?php
 
 $smarty->assign('page', 'contact');
+
+if(isset($_POST['send_message'])){
+    $name = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $mailFrom = $_POST['emailcntc'];
+    $message = $_POST['msgcntc'];
+
+    $mailTo = 'mujeeb.mir8@gmail.com';
+    $headers = 'From:'.$mailFrom;
+    $txt = 'You have received an email from'.$name.'.\n\n'.$message;
+
+    mail($mailTo, $message, $txt, $headers);
+
+    header('Location: ./?mailSend');
+}
