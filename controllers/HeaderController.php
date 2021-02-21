84504628aba68442218539293f6aa4cfe4e41d47
Emilien <emilien.fuchs@gmail.com>
2021-02-21 15:33:26 +0100

    Ajout deconnexion si utilisateur n'existe plus

--- controllers/HeaderController.php
+++ controllers/HeaderController.php
@@ -1,4 +1,15 @@
 <?php
+//Regarde si l'utilisateur existe encore sinon deconnecter le client
+if(isset($_SESSION['utilisateur'])){
+    $utilisateur_session = new Utilisateur();
+    $utilisateur_session = $utilisateur_session->getItem('id_utilisateur',$_SESSION['utilisateur']['id_utilisateur']);
+    if (!$utilisateur_session)
+        header('Location: ./?page=deconnexion');
+    else if ($utilisateur_session->getBanni_Utilisateur() === 1)
+        header('Location: ./?page=deconnexion');
+}
+
+
 $article_header = new Article();
 $utilisateur_header = new Utilisateur();
 $article_header = $article_header->getList('5');
