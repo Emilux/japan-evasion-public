ee3b556da97dac19da54489b8fa5664c104ad0ca
S8s-world <mujeeb.mir8@gmail.com>
2021-02-24 21:00:29 +0100

    Update NewsletterAjax.php
    Adding a mail function for sending messages to subscribers

--- controllers/ajax/NewsletterAjax.php
+++ controllers/ajax/NewsletterAjax.php
@@ -7,6 +7,8 @@ $name_newsletter = $_POST['name'];
 $email_newsletter = $_POST['email'];
 
 
+$msg = "Hi ".$name_newsletter. "\nThank you for subscribing to our newsletter!";
+mail($email_newsletter,"The message", $msg);
 
 $newsletter->setName($name_newsletter);
 
