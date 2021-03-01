3685214b137ebf1dd1c6c0db7f5a658d900ecde3
J-HERTH <73234582+J-HERTH@users.noreply.github.com>
2021-03-01 12:05:41 +0100

    Modif panelAdmin
    Ajout Liste article + Liste commentaire

--- controllers/panel/PanelIndexController.php
+++ controllers/panel/PanelIndexController.php
@@ -12,25 +12,24 @@ if (isset($_SESSION['utilisateur'])) {
 
     $smarty->assign('session', $_SESSION['utilisateur']);
 
-
+    //Compteur 
     $nbUtilisateur = $utilisateur->Count();
-
     $nbCommentaire = $commentaire->Count();
-
     $nbArticle = $article->Count();
-
     $nbArticlePending = $article->Count('statut_article','PENDING');
 
-    //récupérer le contenu de l'utilisateur visionné
+    //récupérer le contenu de l'utilisateur
     $utilisateurs = $utilisateur->getList();
 
-
-
+    //récupérer le contenu d'un commentaire 
+    $commentaires = $commentaire->getList();
+    
+    //récupérer le contenu d'un commentaire 
+    $articles = $article->getList();
 
 
     //récupérer le contenu de l'utilisateur visionné
     $utilisateur = $utilisateur->getItem('id_utilisateur',$_SESSION['utilisateur']['id_utilisateur']); 
-
 
 
     $smarty->assign(array(
@@ -40,6 +39,8 @@ if (isset($_SESSION['utilisateur'])) {
         'nbCommentaire' => $nbCommentaire,
         'nbArticle' => $nbArticle,
         'nbArticlePending' => $nbArticlePending,
+        'commentaires' => $commentaires,
+        'articles' => $articles,
         'role' => $role,
         ));
 
